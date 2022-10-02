46b42aa58fd6a2cd2b8ec310faa6c735efe49300
John White <91273156+BoltR6@users.noreply.github.com>
2022-10-02 14:47:44 +0000

    Added entity selector as well as visualizing code

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 p5.disableFriendlyErrors = true;
 //[temp]
 var ENTITY1;
-//let debug = createGraphics(200,200);
 var SYSTEM = {
 	window: {
 		debug: {
@@ -10,16 +9,20 @@ var SYSTEM = {
 			msgs: [],
 		},
 		code: {
-			x: 400,
-			y: 400,
+			x: 0,
+			y: 300,
+			tabs: ["Entities", "Code", "Item"],
+			selectedTab: "Entities",
+			selectedEntity: undefined,
 		},
 	},
 }
-//var debug
 var MAIN = {
 	globalVariables: {},
 	entities: {},
 };
+
+/** Classes **/
 class Entity {
 	constructor(initialPos, initialRot, initialCode, updateCode) {
 		//Initialize variables all entities share
@@ -32,18 +35,21 @@ class Entity {
 		this.internalVariables = {};
 
 		//Allowing JSONotation to do the heavy lifting of the nested shenanigans
-		this._INITIALCODESTACK = JSON.parse(initialCode);
-		this._UPDATECODESTACK  = JSON.parse(updateCode);
+		this.initialCodeStack = JSON.parse(initialCode);
+		this.updateCodeStack = JSON.parse(updateCode);
+		//De-referencing for to get a mutable version without modifying code permanently
+		this._INITIALCODESTACK = JSON.parse(JSON.stringify(this.initialCodeStack));
+		this._UPDATECODESTACK = JSON.parse(JSON.stringify(this.updateCodeStack));
 
 		//Execute every code statement (ground level, the first blocks)
-		this._INITIALCODESTACK.forEach((item) => {this.EVALUATE_CODE(item);});
+		this._INITIALCODESTACK.forEach((item) => { this.EVALUATE_CODE(item); });
 	}
-	update(){
+	update() {
 		/** Don't implement this until you're there **/
 		//Execute every code statement (ground level, the first blocks)
 		//this._UPDATECODESTACK.forEach((item) => {this.EVALUATE_CODE(item);});
 	}
-	
+
 	/** INTERNAL FUNCTIONS **/
 	//The execution of the string-based code provide by the user.
 	EVALUATE_CODE(CODE_INFO) {
@@ -54,22 +60,23 @@ class Entity {
 		}
 		switch (CODE_INFO[0]) {
 			/** Variable manipulation and retrieval **/
-			case "setIntVar":   return this.setInternalVariable(CODE_INFO[1], CODE_INFO[2]);
-			case "getIntVar": 	return this.getInternalVariable(CODE_INFO[1]);
+			case "setIntVar": return this.setInternalVariable(CODE_INFO[1], CODE_INFO[2]);
+			case "getIntVar": return this.getInternalVariable(CODE_INFO[1]);
 
 			/** Operations **/
 			case "concat": return CODE_INFO.slice(1).join('');
-				
-			case "add":    return this.addAll(CODE_INFO.slice(1));
-			case "sub":    return CODE_INFO[1] * 1 - CODE_INFO[2] * 1;
-			case "mult":   return this.multiplyAll(CODE_INFO.slice(1));
-			case "div":    return (CODE_INFO[1] * 1) / (CODE_INFO[2] * 1);
+
+			case "add": return this.addAll(CODE_INFO.slice(1));
+			case "sub": return CODE_INFO[1] * 1 - CODE_INFO[2] * 1;
+			case "mult": return this.multiplyAll(CODE_INFO.slice(1));
+			case "div": return (CODE_INFO[1] * 1) / (CODE_INFO[2] * 1);
 
 			/** Debug **/
 			case "print": return printMsg(CODE_INFO.slice(1).join(''));
 			default: nonFatalError("Function " + CODE_INFO[0] + " does not exist.");
 		}
 	}
+	//Returns initial code stack for visualization
 
 	/** USER FUNCTIONS **/
 	//Sets variable <name> to <value>
@@ -106,6 +113,35 @@ class Entity {
 	}
 }
 
+/** Graphical Functions **/
+function debugView() {
+	push();
+	translate(SYSTEM.window.debug.x, SYSTEM.window.debug.y);
+	scale(1);
+
+	fill(55);
+	stroke(255);
+	strokeWeight(10);
+	rect(5, 5, 290, 150, 5);
+
+	if (mouseX > SYSTEM.window.debug.x && mouseX < SYSTEM.window.debug.x + 300 && mouseY > SYSTEM.window.debug.y && mouseY < SYSTEM.window.debug.y + 15) {
+		if (mouseIsPressed) {
+			SYSTEM.window.debug.x = mouseX - 150;
+			SYSTEM.window.debug.y = mouseY - 7.5;
+		}
+		fill(110);
+	} else {
+		fill(90);
+	}
+	noStroke();
+	rect(0, 0, 300, 15, 1);
+
+	fill(255);
+	noStroke();
+	text(SYSTEM.window.debug.msgs.join('\n'), 15, 20, 270);
+	pop();
+}
+
 function printMsg(msg) {
 	SYSTEM.window.debug.msgs.push("Message: " + msg);
 }
@@ -115,40 +151,100 @@ function nonFatalError(msg) {
 function setup() {
 	//Initialize sketch
 	createCanvas(windowWidth, windowHeight);
-	background(255,0,0);
+	background(255, 0, 0);
 
-	ENTITY1 = new Entity({}, {}, '[["setIntVar","Health","100"],["print",["concat","You have ",["getIntVar","Health"]," health"]],["setIntVar","Health",["mult",["getIntVar","Health"],1,23]],["print",["concat","You have ",["getIntVar","Health"]," health"]],["print",["mult",3,2,-2]],["nonexistantfunctionshouldthrowwarning","pretendarg1","pretendarg2"]]', "[]");
+	MAIN.entities["Main"] = new Entity({}, {}, '[["setIntVar","Health","100"],["print",["concat","You have ",["getIntVar","Health"]," health"]],["setIntVar","Health",["mult",["getIntVar","Health"],1,23]],["print",["concat","You have ",["getIntVar","Health"]," health"]],["print",["mult",3,2,-2]],["nonexistantfunctionshouldthrowwarning","pretendarg1","pretendarg2"]]', "[]");
+	//MAIN.entities["tempentry"] = "bruh";
+}
+function codeViewTEMP() {
+	fill(55);
+	stroke(255);
+	strokeWeight(10);
+	rect(5, 5, 590, 400, 5);
+
+	if (mouseX > SYSTEM.window.code.x && mouseX < SYSTEM.window.code.x + 600 && mouseY > SYSTEM.window.code.y && mouseY < SYSTEM.window.code.y + 15) {
+		if (mouseIsPressed) {
+			SYSTEM.window.code.x = mouseX - 150;
+			SYSTEM.window.code.y = mouseY - 7.5;
+		}
+		fill(110);
+	} else {
+		fill(90);
+	}
+	noStroke();
+	rect(0, 0, 600, 15, 1);
+
+	SYSTEM.window.code.tabs.forEach((item, ind) => {
+		if((item == "Item" && !SYSTEM.window.code.selectedEntity) || (item == "Code" && !SYSTEM.window.code.selectedEntity)){
+			return;
+		}
+		if (mouseX > SYSTEM.window.code.x + 15 + ind * 50 && mouseX < SYSTEM.window.code.x + 50 + 15 + ind * 50 && mouseY > SYSTEM.window.code.y + 19 && mouseY < SYSTEM.window.code.y + 39) {
+			if (mouseIsPressed) {
+				SYSTEM.window.code.selectedTab = item;
+			}
+			fill(110);
+		} else {
+			fill(90);
+		}
+		strokeWeight(3);
+		stroke(255);
+		rect(15 + ind * 50, 19, 50, 20, 5);
+
+		fill(255);
+		noStroke();
+		textAlign(CENTER);
+		text(item, 40 + ind * 50, 32.5);
+	});
+	fill(90);
+	rect(5,36,590,369,5)
 }
 function draw() {
 	background(70);
-
-	push();
-		translate(SYSTEM.window.debug.x, SYSTEM.window.debug.y);
-		scale(1);
-
-		fill(55);
-		stroke(255);
-		strokeWeight(10);
-		rect(5,5,290,150,5);
-
-		if(mouseX > SYSTEM.window.debug.x && mouseX < SYSTEM.window.debug.x + 300 && mouseY > SYSTEM.window.debug.y && mouseY < SYSTEM.window.debug.y + 15){
-			if(mouseIsPressed){
-				SYSTEM.window.debug.x = mouseX - 150;
-				SYSTEM.window.debug.y = mouseY - 7.5;
-			}
-			fill(110);
-		}else{
-			fill(90);
-		}
-		noStroke();
-		rect(0,0,300,15,1);
-	
-		fill(255);
-		noStroke();
-		text(SYSTEM.window.debug.msgs.join('\n'), 15, 20,270);
-	pop();
-	
 	try {
+		debugView();
+		
+		push();
+			translate(SYSTEM.window.code.x, SYSTEM.window.code.y);
+			scale(1);
+			codeViewTEMP();
+			switch(SYSTEM.window.code.selectedTab){
+				case "Code":MAIN.entities[SYSTEM.window.code.selectedEntity].initialCodeStack.forEach((i,ind)=>{
+						fill(255);
+						noStroke();
+						textSize(12);
+						textAlign(LEFT);
+						text(i[0] + "("+i.slice(1)+")", 20, 63.5 + ind * 15);
+					});
+					break;
+				case "Entities":
+					let ind = -1;
+					fill(255);
+					noStroke();
+					textSize(18);
+					textAlign(LEFT);
+					text("Entity List:", 20, 63.5);
+					
+					for(let key in MAIN.entities){		
+						ind ++;
+						if (mouseX > SYSTEM.window.code.x + 20 && mouseX < SYSTEM.window.code.x + 120 && mouseY > SYSTEM.window.code.y + 73.5 + ind * 50 && mouseY < SYSTEM.window.code.y + 40 + 73.5 + ind * 50) {
+							if (mouseIsPressed) {
+								SYSTEM.window.code.selectedEntity = key;
+							}
+							fill(140);
+						} else {
+							fill(120);
+						}
+						rect(20,73.5 + ind * 50,100,40,4);
+						
+						fill(255);
+						noStroke();
+						textSize(12);
+						textAlign(CENTER);
+						text(key, 70, 97.5 + ind * 50);
+					}
+					break;
+			}
+		pop();
 	} catch (err) {
 		background(120);
 		fill(255);
